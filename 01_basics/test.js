c0c3ea4134bfc9cd07dfa5d3e1125483e79f2d6a
govin07 <141383976+govin07@users.noreply.github.com>
2023-08-19 07:24:24 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log('govind')
+/*const accountId = 144654
+let accountEmail ="govindrajput02653@gmai.com"
+var accountPassword ="123456"
+accountCity ="khandwa"
+
+accountEmail ="sachin@"
+accountPassword ="74747"
+accountCity =" jaipur"
+
+console.table([accountEmail, accountPassword, accountCity]);*/
+let a = 10;
+let b = 20 ;
+a = a+b;
+console.log(a)
+console.log(typeof null);
