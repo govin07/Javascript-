6ff877e1c0189c5a3c9178f74cfc368447288d76
govin07 <govindrajput02653@gmail.com>
2024-09-04 19:52:44 +0530

    revision

--- 08_basics/index.js
+++ 08_basics/index.js
@@ -9,5 +9,42 @@ book.releasedYear = 1923 ;
 book.character1 = "chandar" ;
 book.character2 = "shudaa";
 book.character3 =  "binti";
-console.log(book.author)
+book.content = {};
+book.content.chapter1 = " ilahabaad";
+console.log(book.auconsole.log(aditya > govind);
+let gov = "govind";
+let adi = "aditya";
+gov === adi
+console.log(gov = adi)thor)
 console.log(book);
+
+let govindBD = [0, 6, 0 ,9, 2003 ];
+govindBD = 1 ;
+
+console.log(govindBD);
+console.log([govindBD.length-1])
+
+let count = 1 ;
+count++;
+console.log(count);
+
+count--;
+console.log(count);
+
+count *= 3;
+console.log(count);
+let govind = 20;
+let aditya = 21;
+console.log(govind < aditya);
+console.log(govind > aditya);
+console.log(aditya > govind);
+let govindS = 6000;
+let adityaS = 10000;
+ console.log(adityaS.valueOf !== govindS);
+console.log(aditya > govind);
+let gov = "govind";
+let adi = "aditya";
+gov === adi
+console.log(gov = adi)
+
+
