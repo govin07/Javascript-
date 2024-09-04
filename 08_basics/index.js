5ca370e6694ecb2020b6f6b335ed4d49793eca95
govin07 <govindrajput02653@gmail.com>
2024-09-04 18:12:42 +0530

    object

---
+++ 08_basics/index.js
@@ -0,0 +1,13 @@
+let book ={
+    author : "dharmveer bharti",
+    bookName : "gunaho ka devta",
+    price : 150 ,
+
+}
+book.author = "govind singh solanki"
+book.releasedYear = 1923 ;
+book.character1 = "chandar" ;
+book.character2 = "shudaa";
+book.character3 =  "binti";
+console.log(book.author)
+console.log(book);
