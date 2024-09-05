0437fc3923a15f770dda16c5faeaf27e1e58ffca
govin07 <govindrajput02653@gmail.com>
2024-09-05 18:27:30 +0530

    cm

---
+++ 08_basics/comparison.js
@@ -0,0 +1,8 @@
+let govind = 6000;
+let aditya = 10000;
+ console.log(govind < aditya)
+ console.log("1" > 1);
+ console.log("hello")
+ console.log(null > 0);
+ console.log("govind singh solanki");
+ console.log(undefined <= 0);
