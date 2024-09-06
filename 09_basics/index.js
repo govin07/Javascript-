7e7bc55f930ee9b155c76e4dfd043a23999b9392
govin07 <govindrajput02653@gmail.com>
2024-09-06 21:45:06 +0530

    string

---
+++ 09_basics/index.js
@@ -0,0 +1,7 @@
+const govind = new String("my name is govind singh solanki me badiya gyasur me rahta hu")
+const aditya = "mera naam aditya singh tawar he me pipliya tahar me rahta hu or me ek software developer hu ! thik he ";
+console.log(aditya)
+console.log(govind.toLocaleLowerCase());
+console.log(govind.slice(0, 4))
+console.log(govind.replace("govind", "sand"));
+console.log(aditya.split("mera "));
