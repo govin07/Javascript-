f2addf203fa82547511f6d246e7af247760ac1e8
govin07 <govindrajput02653@gmail.com>
2024-09-23 21:00:48 +0530

    setTimeOut

---
+++ DOM/randomNumber/index.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#input');
+const submit = document.querySelector('button');
+const form = document.querySelector('#form')
+ 
+console.log(form)
+console.log(input)
+console.log(submit)
