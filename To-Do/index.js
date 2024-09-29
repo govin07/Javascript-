aa7a31632870a298a3518b92ca80664e7784463d
govin07 <govindrajput02653@gmail.com>
2024-09-29 21:36:52 +0530

    to do

---
+++ To-Do/index.js
@@ -0,0 +1,32 @@
+const inputBox = document.querySelector('input');
+const buttons = document.querySelector('button');
+const listContainer = document.querySelector('#list-container')
+let button = document.createElement('button');
+
+ buttons.addEventListener('click', function(e){
+  if(inputBox.value === ''){
+    alert("please enter something");
+  }
+  else{
+    let li = document.createElement('li');
+    li.textContent = inputBox.value
+    console.log(li)
+    listContainer.appendChild(li);
+    // let button = document.createElement('button');
+    button.classList.add('button')
+    button.innerHTML = "\u00d7";
+    li.appendChild(button)
+  }
+  inputBox.value = "";
+  savedata()
+
+ })
+ 
+
+//  function savedata(){
+//     localStorage.setItem('data', listContainer.innerHTML)
+//  }
+//  function dataGet(){
+//    listContainer.innerHTML =  localStorage.getItem('data')
+//  }
+//  dataGet()
