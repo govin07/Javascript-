8321cc9c18459e5ff196332414eed3dac540bbcd
govin07 <govindrajput02653@gmail.com>
2024-10-01 16:33:33 +0530

    password Generator

---
+++ password_generator/index.js
@@ -0,0 +1,43 @@
+const passwordText = document.querySelector("#password-space"
+);
+const GenerateBtn = document.querySelector('#btn');
+const copyBtn = document.querySelector('#img');
+
+const lenght = 12;
+let UpperCase = "ABCDEFGHIJKLMNOPQISTUVWXZY";
+let lowerCase = "abcdefghijklmnopqistuvwxyz"
+let number = "0123456789";
+let symbols = "~!@#$%^&*()_+|?><,./";
+let allChats = UpperCase + lowerCase + number + symbols;
+
+GenerateBtn.addEventListener('click',passSection)
+copyBtn.addEventListener('click', textCopy)
+
+
+function passSection(){
+    let password = "";
+    password  += UpperCase[Math.floor(Math.random() * UpperCase.length)];
+    
+    password  += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    
+    password  += number[Math.floor(Math.random() * number.length)];
+    
+    password  += symbols[Math.floor(Math.random() * symbols.length)];
+    
+    while(lenght >= password.length){
+        password += allChats[Math.floor(Math.random() * allChats.length)];
+    }
+    
+    passwordText.value = password;
+    
+}
+
+function textCopy(){
+    passwordText.select();
+    passwordText.setSelectionRange(0, 99999);
+     navigator.clipboard.writeText(passwordText.value)
+     alert(`copied the text ${passwordText.value}`)
+}
+
+
+
