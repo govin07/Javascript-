65185fd86882770313302c376ed5a883a65f29bd
govin07 <govindrajput02653@gmail.com>
2024-10-08 22:34:35 +0530

    recipe app

--- Recipe_App/index.js
+++ Recipe_App/index.js
@@ -5,11 +5,22 @@ const recipeContainer = document.querySelector('.recipe-container')
 
  
     const fetchRecipes = async(query) => {
+        recipeContainer.innerHTML = "fetching recipe...";
     const data =  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
    const response = await data.json();
    console.log(response.meals)
    response.meals.forEach((el)=> {
-    console.log(el)
+    console.log(el.strMealThumb)
+    const div = document.createElement('div');
+    div.classList.add('recipe')
+    div.innerHTML = `<img src=${el.strMealThumb} id="img" alt="404"/>
+    <h3>${el.strMeal}</h3>
+    <p> ${el.strArea}</p>
+    <p> ${el.strCategory}</p>
+    
+    
+    `
+    recipeContainer.appendChild(div)
     
    });
    
